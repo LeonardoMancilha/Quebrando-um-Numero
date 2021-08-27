e05edf551e807503a40ea8dcdb94fb5660ddc9e9
LeonardoMancilha <leonardomachado.sjc@gmail.com>
2021-08-27 09:47:11 -0300

    commit

---
+++ desafio16.py
@@ -0,0 +1,7 @@
+#import math
+#valor = float(input('Digite um número: '))
+#real = math.trunc(valor)
+#print('A parte inteira do numero {} é igual a {}'.format(valor, math.trunc(real)))
+
+x = 'curso de python no cursoemvideo'
+print(f'{x[:5]}')
